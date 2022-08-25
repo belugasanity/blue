925638e2a365b3f54071fd18da224351e87d7b9f
Austin <aust.bagley@gmail.com>
2022-08-24 20:37:19 -0600

    schedule component

---
+++ src/app/schedule/schedule.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-schedule',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.scss']
+})
+export class ScheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

23e74eec21de796718a16145c2d7c54e3c628304
Austin <aust.bagley@gmail.com>
2022-08-24 20:27:04 -0600

    route to services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: AppComponent
+  },
+  {
+    path: 'services',
+    pathMatch: 'full',
+    component: ServicesComponent
   }
 ];
 

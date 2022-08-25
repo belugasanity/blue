85a56b9911d7f00c262e5069a3b64a309ac931eb
Austin <aust.bagley@gmail.com>
2022-08-24 20:29:36 -0600

    route to home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: AppComponent
+    component: HomeComponent
   },
   {
     path: 'services',

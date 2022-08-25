9bc5e1a3666ef88304e235bf3116f7192bc1e544
Austin <aust.bagley@gmail.com>
2022-08-24 20:51:41 -0600

    navigation component

---
+++ src/app/shared/navigation/navigation.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

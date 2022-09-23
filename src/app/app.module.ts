322838ade690e7b07e4241c954394c539b0fd412
Austin <aust.bagley@gmail.com>
2022-09-22 22:38:48 -0600

    move footer template into html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { NavigationComponent } from './shared/navigation/navigation.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NavigationComponent],
+  imports: [BrowserModule, AppRoutingModule, NavigationComponent, FooterComponent],
   providers: [],
   bootstrap: [AppComponent],
 })

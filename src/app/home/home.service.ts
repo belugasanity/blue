e24353a26af5e85c8c3e8590147f68d85c0074ff
Austin <aust.bagley@gmail.com>
2022-10-03 21:03:46 -0600

    create service method to get section 2 data

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,37 @@ import { Injectable } from '@angular/core';
 export class HomeService {
 
   constructor() { }
+
+  getSectionTwo(): Observable<object> {
+    const _data = {
+      highlight: {
+        image: 'src',
+        heading: 'Heating and cooling services in Utah',
+        body: 'Keeping cool in the summer and warm in the winter has never been easier. HVAC repair, replacement and maintenance services near you.'
+      },
+      blocks: [
+        {
+          icon: 'fa-hammer',
+          title: 'HVAC Repair',
+          body: 'Notus Angular comes with a huge number of Fully Coded CSS components.'
+        },
+        {
+          icon: 'fa-toolbox',
+          title: 'HVAC Replacement',
+          body: 'Notus Angular comes with a huge number of Fully Coded CSS components.'
+        },
+        {
+          icon: 'fa-screwdriver-wrench',
+          title: 'Maintenance Services',
+          body: 'Notus Angular comes with a huge number of Fully Coded CSS components.'
+        },
+        {
+          icon: 'fa-fire',
+          title: 'Heating and Cooling',
+          body: 'Notus Angular comes with a huge number of Fully Coded CSS components.'
+        },
+      ]
+    }
+    return of(_data).pipe(delay(500));
+  }
 }
